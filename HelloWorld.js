2747b0ac24614fab847322ff0bcaafcdce5d9bc8
felipeeduardoFE <felipe.eduardo8322@gmail.com>
2025-04-03 16:10:31 -0400

    Aula-01

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,3 +1,3 @@
 //primeiro HelloWorld
 let user = "ola, Felipe"
-console.log(user +",Bem vindo ao mundo da progamação com java script");
+console.log(user +" seja Bem vindo ao mundo da progamação com java script");
