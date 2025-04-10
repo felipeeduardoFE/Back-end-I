d70d61259c1304bd4ead86c3165b6c1aed0e54f2
felipeeduardoFE <felipe.eduardo8322@gmail.com>
2025-04-10 16:14:11 -0400

    03/04/2025

--- exercicio_01.js
+++ exercicio_01.js
@@ -1,3 +1,3 @@
 let nome = "Felipe Eduardo";
-let idade = "15";
+let idade = 15;
 console.log(`olá, meu nome é ${nome} e eu tenho ${idade} anos`);
--- exercicio_02.js
+++ exercicio_02.js
@@ -1,5 +1,4 @@
 let dia = "bom dia";
-let noite = "BOA NOITE"
 
 console.log(dia.toUpperCase());
-console.log(noite.toLowerCase());
+console.log(dia.toLowerCase());
