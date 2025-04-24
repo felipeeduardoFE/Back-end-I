94430d125652941f2473910f03b82c7838c5777b
felipeeduardoFE <felipe.eduardo8322@gmail.com>
2025-04-24 15:41:36 -0400

    24/04/2025

---
+++ Atividades_02/exercicio_01.js
@@ -0,0 +1,12 @@
+let num1 = 7;
+let num2 = 7;
+
+let soma = (num1 + num2);
+let subtração = (num1 - num2);
+let multiplicação = (num1 * num2);
+let divisão = (num1 / num2);
+
+console.log('a soma é ' + soma);
+console.log('a subtração é ' + subtração);
+console.log('a mutiplicação é ' + multiplicação);
+console.log('a divisão é ' + divisão);
---
+++ Atividades_02/exercicio_02.js
@@ -0,0 +1,4 @@
+let idade1 = 20;
+let idade2 = 15;
+comparar = idade1 > idade2
+console.log();
